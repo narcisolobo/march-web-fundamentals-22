32547202a37b9d2e1bb9fe690c5df5ec7058d201
narcisolobo <narcisolobo@gmail.com>
2022-03-22 10:41:38 -0500

    added algo

---
+++ web_fundamentals/algorithms/w1d2_variables_loops.js
@@ -0,0 +1,57 @@
+/* 
+
+Variables and Datatypes in JavaScript
+
+Types of Data
+
+Primitive Datatypes:
+Strings - strings of characters
+Numbers - numbers are numbers no matter if there is a decimal or not
+Booleans - true or false
+
+Rules of naming variable
+1. No spaces
+2. No dashes (hyphens)
+3. You cannot start with a number
+
+The accepted casing of JavaScript is Camel Case - every name begins with a lowercase letter, and every word thereafter begins with an uppercase letter.
+
+*/
+
+// string
+var stuffy = 'koala bear';
+
+// numbers
+var age = 34;
+var percentage = 35.9;
+
+// boolean
+var isRaining = true;
+
+// Keep your code D.R.Y.
+// Don't Repeat Yourself
+
+// For Loop
+// 1. Sentry
+// 2. Where it starts
+// 3. Where it ends
+// 4. How it changes
+
+// boolean statement to tell sentry when to stop
+// boolean statements evaluate to true or false
+// as long as
+
+// shorthand for i = i + 1
+// i += 1
+
+// Order of operations
+// 1. Creation of sentry and value assignment
+// 2. Test the boolean statement, if true, enter the code block
+// 3. Execute the code block
+// 4. Perform the change
+// 5. Go back to step 2 and repeat until false
+
+for (var i = 1; i <= 100; i++) {
+  console.log(i);
+}
+
