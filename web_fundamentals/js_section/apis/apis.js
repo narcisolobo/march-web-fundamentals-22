7317c88a468d2bbb009a325aa2d3c8e1f415615d
narcisolobo <narcisolobo@gmail.com>
2022-03-31 15:31:02 -0500

    added w2d4 notes

---
+++ web_fundamentals/js_section/apis/apis.js
@@ -0,0 +1,9 @@
+// API'S WHAT ARE THEY EVEN?
+// Application Programming Interface
+
+// RESTful API's
+// Representational State Transfer
+
+// Expose resources at endpoints
+
+// JSON - JavaScript Object Notation
